d3f69171114798911da0fcfd7571e30a3f395cd5
Ankit Raj <109663828+ankitrajcs13@users.noreply.github.com>
2025-03-15 21:01:07 +0530

    Update SecurityConfig.java

--- src/main/java/com/irctc2/config/SecurityConfig.java
+++ src/main/java/com/irctc2/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
         http
                 .cors() // Enable CORS
                 .and()
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT is stateless
                 .and()
                 .authorizeRequests()
