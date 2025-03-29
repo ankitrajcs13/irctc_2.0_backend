84106b9ad9a446fa626c9a069ed83d784a0cbbbb
Ankit Raj <109663828+ankitrajcs13@users.noreply.github.com>
2025-03-29 16:54:14 +0530

    Update WebMvcConfig.java

--- src/main/java/com/irctc2/config/WebMvcConfig.java
+++ src/main/java/com/irctc2/config/WebMvcConfig.java
@@ -14,6 +14,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(maintenanceInterceptor)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/api/system-status", "/api/auth/login", "/api/users/register");
+                .excludePathPatterns("/api/system-status", "/api/auth/login", "/api/users/register", "api/system-status");
     }
 }
